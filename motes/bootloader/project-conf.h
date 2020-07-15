45fc52970368d30e804a5ca1d2a2febdafb15f9e
lanpinguo <lanpinguo1985@126.com>
2020-07-15 17:31:32 +0800

    test bootloader ok

--- motes/bootloader/project-conf.h
+++ motes/bootloader/project-conf.h
@@ -32,7 +32,7 @@
 #define PROJECT_CONF_H_
 
 #define FLASH_CONF_FW_ADDR				(CC2538_DEV_FLASH_ADDR + 10*1024)
-#define FLASH_CONF_FW_SIZE 				(8*1024)
+#define FLASH_CONF_FW_SIZE 				(4*1024)   /* 2 Pages, 2K per Page */
 
 
 #endif /*PROJECT_CONF_H_*/
