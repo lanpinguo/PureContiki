f9b510164459b413f9779d143afc24bf54232f86
lanpinguo <lanpinguo1985@126.com>
2020-07-15 20:07:33 +0800

    success to boot app from bootloader

--- platform/pure-cc2538/boot_main.c
+++ platform/pure-cc2538/boot_main.c
@@ -63,7 +63,7 @@
 #include "reg.h"
 #include "ieee-addr.h"
 #include "lpm.h"
-
+#include "flash.h"
 
 #include "xmem.h"
 
@@ -84,8 +84,156 @@
 #define PUTS(s)
 #endif
 /*---------------------------------------------------------------------------*/
+/* ------------------------------------------------------------------------------------------------
+ *                                          Typedefs
+ * ------------------------------------------------------------------------------------------------
+ */
+	// OTA Header constants
+#define OTA_HDR_MAGIC_NUMBER                0x0BEEF11E
+#define OTA_HDR_BLOCK_SIZE                  128
+#define OTA_HDR_STACK_VERSION               2
+#define OTA_HDR_HEADER_VERSION              0x0100
+#define OTA_HDR_FIELD_CTRL                  0
+	
+#define OTA_HEADER_LEN_MIN                  56
+#define OTA_HEADER_LEN_MAX                  69
+#define OTA_HEADER_LEN_MIN_ECDSA            166
+#define OTA_HEADER_STR_LEN                  32
+	
+#define OTA_HEADER_IMG_VALID              	0x5A5A
+	
+#define OTA_FC_SCV_PRESENT                  (0x1 << 0)
+#define OTA_FC_DSF_PRESENT                  (0x1 << 1)
+#define OTA_FC_HWV_PRESENT                  (0x1 << 2)
+
+	
+#define HAL_IBM_LEDGER_PAGE        			254
+
+#define NVIC_VECTOR_SIZE					(163*4)
+	
+	
+#define GPIO_A_DIR				0x400D9400
+#define GPIO_A_DATA				0x400D9000
+#define GPIO_C_DIR				0x400DB400
+#define GPIO_C_DATA				0x400DB000
+
+#define HWREG(x)                                                              \
+	(*((volatile unsigned long *)(x)))
+
+ 
+typedef struct
+{
+	uint16_t manufacturer;
+	uint16_t type;
+	uint32_t version;
+} zclOTA_FileID_t;
+
+typedef struct
+{
+	uint16_t tag;
+	uint32_t length;
+} OTA_SubElementHdr_t;
+
+typedef struct
+{
+	uint32_t magicNumber;
+	uint32_t imageSize;
+	uint16_t imageValid;
+	uint16_t headerLength;
+	uint16_t headerVersion;
+	uint16_t fieldControl;
+	zclOTA_FileID_t fileId;
+}__attribute__ ((packed)) OTA_ImageHeader_t;
+
+typedef struct {
+  uint32_t stackPtr;
+  uint32_t nvicReset;
+} ibm_ledger_t;
+//static_assert((sizeof(ibm_ledger_t) == 16), "Need to PACK the ibm_ledger_t");
+
+char test[] = "hello world\r\n";
+char no_img_found[] = "no image found\r\n";
+char img_found[] = "found image:\r\n";
 
 
+
+
+void EnterNvmApplication(uint32_t spInit, uint32_t resetVector)
+{
+	// Set the LR register to a value causing a processor fault in case of
+	// an accidental return from the application.
+	asm("mov r2, #0xffffffff");
+	asm("mov lr, r2");
+
+	asm("mov sp, r0");	// Setup the initial stack pointer value.
+
+	asm("bx r1");  // Branch to application reset ISR.
+}
+
+void uart_write_byte(uint8_t uart, uint8_t b);
+
+
+int dbg_output(char * buf, unsigned int len)
+{
+	int i = 0;
+
+	for(i = 0; i < len ; i++){
+		uart_write_byte(0, buf[i]);
+	}
+	return i;
+}
+
+int boot_app(void)
+{
+	volatile uint32_t ledgerPageAddr = FLASH_FW_ADDR;
+
+	// Set direction output and initial value for PC2 and PC0
+	// Greed LED on PA2
+	// Red LED on PA4
+	// Blue LED on PA5
+	HWREG(GPIO_A_DIR) |= 0x34;
+	HWREG(GPIO_A_DATA + (0x34 << 2)) = 0;
+	
+	//xmem_init();
+
+	
+
+	for (int pgCnt = 0; pgCnt < HAL_IBM_LEDGER_PAGE; pgCnt++, ledgerPageAddr += FLASH_PAGE_SIZE)
+	{
+		OTA_ImageHeader_t *pLedger = (OTA_ImageHeader_t *)(ledgerPageAddr + NVIC_VECTOR_SIZE);
+
+		if (pLedger->magicNumber != OTA_HDR_MAGIC_NUMBER)
+		{
+			continue;
+		}
+		// Turn on both LED's.
+		HWREG(GPIO_A_DATA + (0x30 << 2)) = 0x30;
+
+		if (pLedger->imageValid == OTA_HEADER_IMG_VALID)
+		{
+			dbg_output(img_found,sizeof(img_found));
+			dbg_output((char*)&pLedger->headerLength,6);
+			dbg_output("\r\n",2);
+			dbg_output("on page:",sizeof("on page:"));
+			dbg_output((char*)&pgCnt,4);
+			dbg_output("\r\n",2);
+			HWREG(GPIO_A_DATA + (0x04 << 2)) = 0x04;
+			ibm_ledger_t* img = (ibm_ledger_t*)(ledgerPageAddr);
+			// Sanity check NVIC entries.
+			if ((img->stackPtr > 0x20000000) &&
+					(img->stackPtr < 0x27007FFF) &&
+					(img->nvicReset > 0x200000) &&
+					(img->nvicReset < 0x0027EFFF))
+			{
+				HWREG(GPIO_A_DATA + (0x20 << 2)) = 0x00;
+				EnterNvmApplication(img->stackPtr, img->nvicReset);
+			}
+		}
+	}
+
+
+	return -1;
+}
 
 /*---------------------------------------------------------------------------*/
 /**
@@ -97,27 +245,17 @@ void debug_led(void)
 	volatile unsigned long ulLoopCount;
 	REG(GPIO_A_BASE + GPIO_DIR) = 0x04; /* PA2 output*/
 
-	// Loop forever.
-	while(1)
-	{
-			// Turn Blue	LED.
-			REG(GPIO_A_BASE + GPIO_DATA + (0x04 << 2)) ^= 0x04;
+	// Turn Blue	LED.
+	REG(GPIO_A_BASE + GPIO_DATA + (0x04 << 2)) ^= 0x04;
 
-			// Delay for a bit
-			for(ulLoopCount = 200000; ulLoopCount > 0; ulLoopCount--)
-			{
-			}
-	};
+	// Delay for a bit
+	for(ulLoopCount = 200000; ulLoopCount > 0; ulLoopCount--);
 }
 
-void
-uart_write_byte(uint8_t uart, uint8_t b);
  
-int
-main(void)
+int main(void)
 {
-	char test[] = "hello world\r\n";
-	int i = 0;
+	int rc = 0;
 	
 	nvic_init();
 	
@@ -127,32 +265,25 @@ main(void)
 	//lpm_init();
 	//rtimer_init();
 	//gpio_init();
-
-
-
-
 	//watchdog_init();
 
 	uart_init(0);
 
-
-
 	xmem_init();
-
-
 
 
 	//watchdog_start();
 	//fade(LEDS_ORANGE);
 
-
+	rc = boot_app();
+	if(rc != 0){
+		dbg_output(no_img_found,sizeof(no_img_found));
+	}
 
 	while(1) {
 
-		for(i = 0; test[i] != 0; i++){
-			uart_write_byte(0, test[i]);
-		}
-
+		dbg_output(test,sizeof(test));
+		debug_led();
 		/* We have serviced all pending events. Enter a Low-Power mode. */
 		lpm_enter();
 	}
